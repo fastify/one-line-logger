e5bc40f847b964d2d585c0625529078145baed16
TommyDew42 <tommydew42@gmail.com>
2022-08-11 22:29:10 +0800

    add eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    'jest/globals': true
+  },
+  extends: ['standard'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  rules: {},
+  ignorePatterns: ['dist/']
+}
