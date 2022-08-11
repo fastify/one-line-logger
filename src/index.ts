5dafcd044a67fe21ecb90cf683be9af93b9162b0
TommyDew42 <tommydew42@gmail.com>
2022-08-11 22:30:01 +0800

    main index.ts

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import pretty from 'pino-pretty'
+
+const LEVEL_TO_STRING = {
+  60: 'fatal',
+  50: 'error',
+  40: 'warn',
+  30: 'info',
+  20: 'debug',
+  10: 'trace'
+}
+
+export interface Request {
+  method: string
+  url: string
+}
+
+export type LogDescriptor = Record<string, unknown> & {
+  time: number
+  level: number
+  req?: Request
+}
+
+export const messageFormat = (
+  log: LogDescriptor,
+  messageKey: string
+): string => {
+  const date = new Date(log.time)
+  const time = `${date.toLocaleTimeString()}.${date.getMilliseconds()}`
+  const level = LEVEL_TO_STRING[log.level]
+
+  const logMessages = [time, level]
+
+  if (log.req) {
+    const { method, url } = log.req
+
+    logMessages.push(`${method} ${url}`)
+  }
+
+  logMessages.push(log[messageKey])
+
+  return logMessages.join(' - ')
+}
+
+const target = (opts: pretty.PrettyOptions) =>
+  pretty({
+    messageFormat,
+    ignore: 'pid,hostname,time,level',
+    hideObject: true,
+    ...opts
+  })
+
+export default target
