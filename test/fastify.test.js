6c55ee593165473a60c3c1ab911ed43f8d70988e
Cristian Barlutiu <cristian.barlutiu@gmail.com>
2024-05-22 08:59:03 +0200

    fixed linting

--- test/fastify.test.js
+++ test/fastify.test.js
@@ -1,79 +1,79 @@
-'use strict';
+'use strict'
 
-const { serverFactory, TIME, unmockTime, mockTime } = require('./helpers');
-const tap = require('tap');
+const { serverFactory, TIME, unmockTime, mockTime } = require('./helpers')
+const tap = require('tap')
 
-const { test } = tap;
+const { test } = tap
 
-const messages = [];
-let server = serverFactory(messages, { colorize: false });
+const messages = []
+let server = serverFactory(messages, { colorize: false })
 
 tap.before(() => {
-  mockTime();
-});
+  mockTime()
+})
 
 tap.teardown(() => {
-  unmockTime();
-});
+  unmockTime()
+})
 
 tap.beforeEach(() => {
   // empty messages array
-  messages.splice(0, messages.length);
+  messages.splice(0, messages.length)
 
-  server = serverFactory(messages);
-});
+  server = serverFactory(messages)
+})
 
 test('should log server started messages', async (t) => {
-  await server.listen({ port: 63995 });
+  await server.listen({ port: 63995 })
 
   const messagesExpected = [
     `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36mServer listening at http://127.0.0.1:63995\x1B[39m\n`,
-    `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36mServer listening at http://[::1]:63995\x1B[39m\n`,
-  ];
+    `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36mServer listening at http://[::1]:63995\x1B[39m\n`
+  ]
 
   // sort because the order of the messages is not guaranteed
-  t.same(messages.sort(), messagesExpected.sort());
-  await server.close();
-  t.end();
-});
+  t.same(messages.sort(), messagesExpected.sort())
+  await server.close()
+  t.end()
+})
 
-const methods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'];
+const methods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD']
 methods.forEach((method) => {
   test('should log request and response messages for %p', async (t) => {
-    const serverMethod = method === 'HEAD' ? 'GET' : method;
+    const serverMethod = method === 'HEAD' ? 'GET' : method
     server[serverMethod.toLowerCase()]('/path', (_, req) => {
-      req.send();
-    });
+      req.send()
+    })
 
     await server.inject({
       method,
-      url: '/path',
-    });
+      url: '/path'
+    })
 
     const messagesExpected = [
       `${TIME} - \x1B[32minfo\x1B[39m - ${method} /path - \x1B[36mincoming request\x1B[39m\n`,
-      `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36mrequest completed\x1B[39m\n`,
-    ];
-    t.same(messages, messagesExpected);
-    t.end();
-  });
-});
+      `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36mrequest completed\x1B[39m\n`
+    ]
+    t.same(messages, messagesExpected)
+    t.end()
+  })
+})
 
 test('should handle user defined log', async (t) => {
-  server = serverFactory(messages, { minimumLevel: 'trace' });
+  server = serverFactory(messages, { minimumLevel: 'trace' })
 
   server.get('/a-path-with-user-defined-log', (res, req) => {
-    res.log.fatal('a user defined fatal log');
-    res.log.error('a user defined error log');
-    res.log.warn('a user defined warn log');
-    res.log.info('a user defined info log');
-    res.log.debug('a user defined debug log');
-    res.log.trace('a user defined trace log');
+    res.log.fatal('a user defined fatal log')
+    res.log.error('a user defined error log')
+    res.log.warn('a user defined warn log')
+    res.log.info('a user defined info log')
+    res.log.debug('a user defined debug log')
+    res.log.trace('a user defined trace log')
 
-    req.send();
-  });
+    req.send()
+  })
 
-  await server.inject('/a-path-with-user-defined-log');
+  await server.inject('/a-path-with-user-defined-log')
 
   const messagesExpected = [
     `${TIME} - \x1B[32minfo\x1B[39m - GET /a-path-with-user-defined-log - \x1B[36mincoming request\x1B[39m\n`,
@@ -83,8 +83,8 @@ test('should handle user defined log', async (t) => {
     `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36ma user defined info log\x1B[39m\n`,
     `${TIME} - \x1B[34mdebug\x1B[39m - \x1B[36ma user defined debug log\x1B[39m\n`,
     `${TIME} - \x1B[90mtrace\x1B[39m - \x1B[36ma user defined trace log\x1B[39m\n`,
-    `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36mrequest completed\x1B[39m\n`,
-  ];
-  t.same(messages, messagesExpected);
-  t.end();
-});
+    `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36mrequest completed\x1B[39m\n`
+  ]
+  t.same(messages, messagesExpected)
+  t.end()
+})
--- test/index.test.js
+++ test/index.test.js
@@ -1,33 +1,33 @@
-'use strict';
+'use strict'
 
-const { EPOCH, TIME, MESSAGE_KEY, mockTime, unmockTime } = require('./helpers');
-const target = require('..');
-const tap = require('tap');
+const { EPOCH, TIME, MESSAGE_KEY, mockTime, unmockTime } = require('./helpers')
+const target = require('..')
+const tap = require('tap')
 
-const { messageFormatFactory } = target;
+const { messageFormatFactory } = target
 
-const { test } = tap;
+const { test } = tap
 
-const messageFormat = messageFormatFactory();
+const messageFormat = messageFormatFactory()
 
 tap.before(() => {
-  mockTime();
-});
+  mockTime()
+})
 
 tap.teardown(() => {
-  unmockTime();
-});
+  unmockTime()
+})
 
 test('able to instantiate target without arguments', (t) => {
-  target();
-  t.pass();
-  t.end();
-});
+  target()
+  t.pass()
+  t.end()
+})
 
 const logDescriptorLogPairs = [
   [
     { time: EPOCH, level: 10, [MESSAGE_KEY]: 'basic log' },
-    `${TIME} - \x1B[90mtrace\x1B[39m - \x1B[36mbasic log\x1B[39m`,
+    `${TIME} - \x1B[90mtrace\x1B[39m - \x1B[36mbasic log\x1B[39m`
   ],
   [
     {
@@ -36,24 +36,24 @@ const logDescriptorLogPairs = [
       [MESSAGE_KEY]: 'basic incoming request log',
       req: {
         method: 'GET',
-        url: '/path',
-      },
+        url: '/path'
+      }
     },
-    `${TIME} - \x1B[32minfo\x1B[39m - GET /path - \x1B[36mbasic incoming request log\x1B[39m`,
-  ],
-];
+    `${TIME} - \x1B[32minfo\x1B[39m - GET /path - \x1B[36mbasic incoming request log\x1B[39m`
+  ]
+]
 logDescriptorLogPairs.forEach(([logDescriptor, expectedLog]) => {
   test('format log correctly with different logDescriptor', (t) => {
-    const log = messageFormat(logDescriptor, MESSAGE_KEY);
-    t.equal(log, expectedLog);
-    t.end();
-  });
-});
+    const log = messageFormat(logDescriptor, MESSAGE_KEY)
+    t.equal(log, expectedLog)
+    t.end()
+  })
+})
 
 const logDescriptorColorizedLogPairs = [
   [
     { time: EPOCH, level: 10, [MESSAGE_KEY]: 'basic log' },
-    `${TIME} - \u001B[90mtrace\u001B[39m - \u001B[36mbasic log\u001B[39m`,
+    `${TIME} - \u001B[90mtrace\u001B[39m - \u001B[36mbasic log\u001B[39m`
   ],
   [
     {
@@ -62,32 +62,32 @@ const logDescriptorColorizedLogPairs = [
       [MESSAGE_KEY]: 'basic incoming request log',
       req: {
         method: 'GET',
-        url: '/path',
-      },
+        url: '/path'
+      }
     },
-    `${TIME} - \u001B[32minfo\u001B[39m - GET /path - \u001B[36mbasic incoming request log\u001B[39m`,
-  ],
-];
+    `${TIME} - \u001B[32minfo\u001B[39m - GET /path - \u001B[36mbasic incoming request log\u001B[39m`
+  ]
+]
 logDescriptorColorizedLogPairs.forEach(([logDescriptor, logColorized]) => {
   test('colorize log correctly with different logDescriptor', (t) => {
-    const log = messageFormat(logDescriptor, MESSAGE_KEY);
+    const log = messageFormat(logDescriptor, MESSAGE_KEY)
 
-    t.equal(log, logColorized);
-    t.end();
-  });
-});
+    t.equal(log, logColorized)
+    t.end()
+  })
+})
 
 {
   const levels = {
     foo: 35,
-    bar: 45,
-  };
-  const messageFormat = messageFormatFactory(levels);
+    bar: 45
+  }
+  const messageFormat = messageFormatFactory(levels)
 
   const logCustomLevelsLogPairs = [
     [
       { time: EPOCH, level: 35, [MESSAGE_KEY]: 'basic foo log' },
-      `${TIME} - \u001b[37mfoo\u001b[39m - \u001B[36mbasic foo log\u001B[39m`,
+      `${TIME} - \u001b[37mfoo\u001b[39m - \u001B[36mbasic foo log\u001B[39m`
     ],
     [
       {
@@ -96,35 +96,35 @@ logDescriptorColorizedLogPairs.forEach(([logDescriptor, logColorized]) => {
         [MESSAGE_KEY]: 'basic incoming request bar log',
         req: {
           method: 'GET',
-          url: '/bar',
-        },
+          url: '/bar'
+        }
       },
-      `${TIME} - \u001b[37mbar\u001b[39m - GET /bar - \u001B[36mbasic incoming request bar log\u001B[39m`,
-    ],
-  ];
+      `${TIME} - \u001b[37mbar\u001b[39m - GET /bar - \u001B[36mbasic incoming request bar log\u001B[39m`
+    ]
+  ]
   logCustomLevelsLogPairs.forEach(([logDescriptor, expectedLog]) => {
     test('format log correctly with custom levels', (t) => {
-      const log = messageFormat(logDescriptor, MESSAGE_KEY);
-      t.equal(log, expectedLog);
-      t.end();
-    });
-  });
+      const log = messageFormat(logDescriptor, MESSAGE_KEY)
+      t.equal(log, expectedLog)
+      t.end()
+    })
+  })
 }
 
 {
   const levels = {
     foo: 35,
-    bar: 45,
-  };
+    bar: 45
+  }
   const messageFormat = messageFormatFactory(levels, {
     35: 'bgCyanBright',
-    45: 'yellow',
-  });
+    45: 'yellow'
+  })
 
   const logCustomLevelsLogPairs = [
     [
       { time: EPOCH, level: 35, [MESSAGE_KEY]: 'basic foo log' },
-      `${TIME} - \u001B[106mfoo\u001B[49m - \u001B[36mbasic foo log\u001B[39m`,
+      `${TIME} - \u001B[106mfoo\u001B[49m - \u001B[36mbasic foo log\u001B[39m`
     ],
     [
       {
@@ -133,17 +133,17 @@ logDescriptorColorizedLogPairs.forEach(([logDescriptor, logColorized]) => {
         [MESSAGE_KEY]: 'basic incoming request bar log',
         req: {
           method: 'GET',
-          url: '/bar',
-        },
+          url: '/bar'
+        }
       },
-      `${TIME} - \u001B[33mbar\u001B[39m - GET /bar - \u001B[36mbasic incoming request bar log\u001B[39m`,
-    ],
-  ];
+      `${TIME} - \u001B[33mbar\u001B[39m - GET /bar - \u001B[36mbasic incoming request bar log\u001B[39m`
+    ]
+  ]
   logCustomLevelsLogPairs.forEach(([logDescriptor, expectedLog]) => {
     test('format log correctly with custom colors per level', (t) => {
-      const log = messageFormat(logDescriptor, MESSAGE_KEY);
-      t.equal(log, expectedLog);
-      t.end();
-    });
-  });
+      const log = messageFormat(logDescriptor, MESSAGE_KEY)
+      t.equal(log, expectedLog)
+      t.end()
+    })
+  })
 }
