d56afb29b8a0fc6b11684403388439655a064488
Cristian Barlutiu <cristian.barlutiu@gmail.com>
2024-05-24 11:55:32 +0200

    fixed linting

--- test/fastify.test.js
+++ test/fastify.test.js
@@ -1,45 +1,45 @@
-'use strict';
+'use strict'
 
-const { serverFactory, TIME, unmockTime, mockTime } = require('./helpers');
-const pretty = require('pino-pretty');
-const tap = require('tap');
+const { serverFactory, TIME, unmockTime, mockTime } = require('./helpers')
+const pretty = require('pino-pretty')
+const tap = require('tap')
 
-const { test } = tap;
+const { test } = tap
 
-const messages = [];
-let server = serverFactory(messages, { colorize: false });
+const messages = []
+let server = serverFactory(messages, { colorize: false })
 
 tap.before(() => {
-  mockTime();
-});
+  mockTime()
+})
 
 tap.teardown(() => {
-  unmockTime();
-});
+  unmockTime()
+})
 
 tap.beforeEach(() => {
   // empty messages array
-  messages.splice(0, messages.length);
+  messages.splice(0, messages.length)
 
-  server = serverFactory(messages);
-});
+  server = serverFactory(messages)
+})
 
 test('should log server started messages', async (t) => {
   t.beforeEach(async () => {
-    await server.listen({ port: 63995 });
-    t.teardown(async () => await server.close());
-  });
+    await server.listen({ port: 63995 })
+    t.teardown(async () => await server.close())
+  })
 
   t.test('colors supported in TTY', { skip: !pretty.isColorSupported }, (t) => {
     const messagesExpected = [
       `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36mServer listening at http://127.0.0.1:63995\x1B[39m\n`,
-      `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36mServer listening at http://[::1]:63995\x1B[39m\n`,
-    ];
+      `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36mServer listening at http://[::1]:63995\x1B[39m\n`
+    ]
 
     // sort because the order of the messages is not guaranteed
-    t.same(messages.sort(), messagesExpected.sort());
-    t.end();
-  });
+    t.same(messages.sort(), messagesExpected.sort())
+    t.end()
+  })
 
   t.test(
     'colors not supported in TTY',
@@ -47,30 +47,30 @@ test('should log server started messages', async (t) => {
     (t) => {
       const messagesExpected = [
         `${TIME} - info - Server listening at http://127.0.0.1:63995\n`,
-        `${TIME} - info - Server listening at http://[::1]:63995\n`,
-      ];
+        `${TIME} - info - Server listening at http://[::1]:63995\n`
+      ]
 
       // sort because the order of the messages is not guaranteed
-      t.same(messages.sort(), messagesExpected.sort());
-      t.end();
+      t.same(messages.sort(), messagesExpected.sort())
+      t.end()
     }
-  );
-});
+  )
+})
 
-const methods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'];
+const methods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD']
 methods.forEach((method) => {
   test('should log request and response messages for %p', async (t) => {
     t.beforeEach(async () => {
-      const serverMethod = method === 'HEAD' ? 'GET' : method;
+      const serverMethod = method === 'HEAD' ? 'GET' : method
       server[serverMethod.toLowerCase()]('/path', (_, req) => {
-        req.send();
-      });
+        req.send()
+      })
 
       await server.inject({
         method,
-        url: '/path',
-      });
-    });
+        url: '/path'
+      })
+    })
 
     t.test(
       'colors supported in TTY',
@@ -78,12 +78,12 @@ methods.forEach((method) => {
       (t) => {
         const messagesExpected = [
           `${TIME} - \x1B[32minfo\x1B[39m - ${method} /path - \x1B[36mincoming request\x1B[39m\n`,
-          `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36mrequest completed\x1B[39m\n`,
-        ];
-        t.same(messages, messagesExpected);
-        t.end();
+          `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36mrequest completed\x1B[39m\n`
+        ]
+        t.same(messages, messagesExpected)
+        t.end()
       }
-    );
+    )
 
     t.test(
       'colors not supported in TTY',
@@ -91,32 +91,32 @@ methods.forEach((method) => {
       (t) => {
         const messagesExpected = [
           `${TIME} - info - ${method} /path - incoming request\n`,
-          `${TIME} - info - request completed\n`,
-        ];
-        t.same(messages, messagesExpected);
-        t.end();
+          `${TIME} - info - request completed\n`
+        ]
+        t.same(messages, messagesExpected)
+        t.end()
       }
-    );
-  });
-});
+    )
+  })
+})
 
 test('should handle user defined log', async (t) => {
   t.beforeEach(async () => {
-    server = serverFactory(messages, { minimumLevel: 'trace' });
+    server = serverFactory(messages, { minimumLevel: 'trace' })
 
     server.get('/a-path-with-user-defined-log', (res, req) => {
-      res.log.fatal('a user defined fatal log');
-      res.log.error('a user defined error log');
-      res.log.warn('a user defined warn log');
-      res.log.info('a user defined info log');
-      res.log.debug('a user defined debug log');
-      res.log.trace('a user defined trace log');
+      res.log.fatal('a user defined fatal log')
+      res.log.error('a user defined error log')
+      res.log.warn('a user defined warn log')
+      res.log.info('a user defined info log')
+      res.log.debug('a user defined debug log')
+      res.log.trace('a user defined trace log')
 
-      req.send();
-    });
+      req.send()
+    })
 
-    await server.inject('/a-path-with-user-defined-log');
-  });
+    await server.inject('/a-path-with-user-defined-log')
+  })
 
   t.test('colors supported in TTY', { skip: !pretty.isColorSupported }, (t) => {
     const messagesExpected = [
@@ -127,11 +127,11 @@ test('should handle user defined log', async (t) => {
       `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36ma user defined info log\x1B[39m\n`,
       `${TIME} - \x1B[34mdebug\x1B[39m - \x1B[36ma user defined debug log\x1B[39m\n`,
       `${TIME} - \x1B[90mtrace\x1B[39m - \x1B[36ma user defined trace log\x1B[39m\n`,
-      `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36mrequest completed\x1B[39m\n`,
-    ];
-    t.same(messages, messagesExpected);
-    t.end();
-  });
+      `${TIME} - \x1B[32minfo\x1B[39m - \x1B[36mrequest completed\x1B[39m\n`
+    ]
+    t.same(messages, messagesExpected)
+    t.end()
+  })
 
   t.test(
     'colors not supported in TTY',
@@ -145,10 +145,10 @@ test('should handle user defined log', async (t) => {
         `${TIME} - info - a user defined info log\n`,
         `${TIME} - debug - a user defined debug log\n`,
         `${TIME} - trace - a user defined trace log\n`,
-        `${TIME} - info - request completed\n`,
-      ];
-      t.same(messages, messagesExpected);
-      t.end();
+        `${TIME} - info - request completed\n`
+      ]
+      t.same(messages, messagesExpected)
+      t.end()
     }
-  );
-});
+  )
+})
--- test/index.test.js
+++ test/index.test.js
@@ -1,36 +1,36 @@
-'use strict';
+'use strict'
 
-const { EPOCH, TIME, MESSAGE_KEY, mockTime, unmockTime } = require('./helpers');
-const target = require('..');
-const tap = require('tap');
-const pretty = require('pino-pretty');
+const { EPOCH, TIME, MESSAGE_KEY, mockTime, unmockTime } = require('./helpers')
+const target = require('..')
+const tap = require('tap')
+const pretty = require('pino-pretty')
 
-const { messageFormatFactory } = target;
+const { messageFormatFactory } = target
 
-const { test } = tap;
+const { test } = tap
 
-const messageFormat = messageFormatFactory();
+const messageFormat = messageFormatFactory()
 
 tap.before(() => {
-  mockTime();
-});
+  mockTime()
+})
 
 tap.teardown(() => {
-  unmockTime();
-});
+  unmockTime()
+})
 
 test('able to instantiate target without arguments', (t) => {
-  target();
-  t.pass();
-  t.end();
-});
+  target()
+  t.pass()
+  t.end()
+})
 
 test('format log correctly with different logDescriptor', async (t) => {
   const logDescriptorLogPairs = [
     [
       { time: EPOCH, level: 10, [MESSAGE_KEY]: 'basic log' },
       `${TIME} - \x1B[90mtrace\x1B[39m - \x1B[36mbasic log\x1B[39m`,
-      `${TIME} - trace - basic log`,
+      `${TIME} - trace - basic log`
     ],
     [
       {
@@ -39,13 +39,13 @@ test('format log correctly with different logDescriptor', async (t) => {
         [MESSAGE_KEY]: 'basic incoming request log',
         req: {
           method: 'GET',
-          url: '/path',
-        },
+          url: '/path'
+        }
       },
       `${TIME} - \x1B[32minfo\x1B[39m - GET /path - \x1B[36mbasic incoming request log\x1B[39m`,
-      `${TIME} - info - GET /path - basic incoming request log`,
-    ],
-  ];
+      `${TIME} - info - GET /path - basic incoming request log`
+    ]
+  ]
 
   await logDescriptorLogPairs.forEach(
     async ([logDescriptor, expectedLogColored, expectedLogUncolored]) => {
@@ -53,31 +53,31 @@ test('format log correctly with different logDescriptor', async (t) => {
         'colors supported in TTY',
         { skip: !pretty.isColorSupported },
         (t) => {
-          const log = messageFormat(logDescriptor, MESSAGE_KEY);
-          t.equal(log, expectedLogColored);
-          t.end();
+          const log = messageFormat(logDescriptor, MESSAGE_KEY)
+          t.equal(log, expectedLogColored)
+          t.end()
         }
-      );
+      )
 
       await t.test(
         'colors not supported in TTY',
         { skip: pretty.isColorSupported },
         (t) => {
-          const log = messageFormat(logDescriptor, MESSAGE_KEY);
-          t.equal(log, expectedLogUncolored);
-          t.end();
+          const log = messageFormat(logDescriptor, MESSAGE_KEY)
+          t.equal(log, expectedLogUncolored)
+          t.end()
         }
-      );
+      )
     }
-  );
-});
+  )
+})
 
 test('colorize log correctly with different logDescriptor', async (t) => {
   const logDescriptorColorizedLogPairs = [
     [
       { time: EPOCH, level: 10, [MESSAGE_KEY]: 'basic log' },
       `${TIME} - \u001B[90mtrace\u001B[39m - \u001B[36mbasic log\u001B[39m`,
-      `${TIME} - trace - basic log`,
+      `${TIME} - trace - basic log`
     ],
     [
       {
@@ -86,13 +86,13 @@ test('colorize log correctly with different logDescriptor', async (t) => {
         [MESSAGE_KEY]: 'basic incoming request log',
         req: {
           method: 'GET',
-          url: '/path',
-        },
+          url: '/path'
+        }
       },
       `${TIME} - \u001B[32minfo\u001B[39m - GET /path - \u001B[36mbasic incoming request log\u001B[39m`,
-      `${TIME} - info - GET /path - basic incoming request log`,
-    ],
-  ];
+      `${TIME} - info - GET /path - basic incoming request log`
+    ]
+  ]
 
   await logDescriptorColorizedLogPairs.forEach(
     async ([logDescriptor, expectedLogColored, expectedLogUncolored]) => {
@@ -100,37 +100,37 @@ test('colorize log correctly with different logDescriptor', async (t) => {
         'colors supported in TTY',
         { skip: !pretty.isColorSupported },
         (t) => {
-          const log = messageFormat(logDescriptor, MESSAGE_KEY);
-          t.equal(log, expectedLogColored);
-          t.end();
+          const log = messageFormat(logDescriptor, MESSAGE_KEY)
+          t.equal(log, expectedLogColored)
+          t.end()
         }
-      );
+      )
 
       await t.test(
         'colors not supported in TTY',
         { skip: pretty.isColorSupported },
         (t) => {
-          const log = messageFormat(logDescriptor, MESSAGE_KEY);
-          t.equal(log, expectedLogUncolored);
-          t.end();
+          const log = messageFormat(logDescriptor, MESSAGE_KEY)
+          t.equal(log, expectedLogUncolored)
+          t.end()
         }
-      );
+      )
     }
-  );
-});
+  )
+})
 
 test('format log correctly with custom levels', async (t) => {
   const levels = {
     foo: 35,
-    bar: 45,
-  };
-  const messageFormat = messageFormatFactory(levels);
+    bar: 45
+  }
+  const messageFormat = messageFormatFactory(levels)
 
   const logCustomLevelsLogPairs = [
     [
       { time: EPOCH, level: 35, [MESSAGE_KEY]: 'basic foo log' },
       `${TIME} - \u001b[37mfoo\u001b[39m - \u001B[36mbasic foo log\u001B[39m`,
-      `${TIME} - foo - basic foo log`,
+      `${TIME} - foo - basic foo log`
     ],
     [
       {
@@ -139,13 +139,13 @@ test('format log correctly with custom levels', async (t) => {
         [MESSAGE_KEY]: 'basic incoming request bar log',
         req: {
           method: 'GET',
-          url: '/bar',
-        },
+          url: '/bar'
+        }
       },
       `${TIME} - \u001b[37mbar\u001b[39m - GET /bar - \u001B[36mbasic incoming request bar log\u001B[39m`,
-      `${TIME} - bar - GET /bar - basic incoming request bar log`,
-    ],
-  ];
+      `${TIME} - bar - GET /bar - basic incoming request bar log`
+    ]
+  ]
 
   await logCustomLevelsLogPairs.forEach(
     async ([logDescriptor, expectedLogColored, expectedLogUncolored]) => {
@@ -153,40 +153,40 @@ test('format log correctly with custom levels', async (t) => {
         'colors supported in TTY',
         { skip: !pretty.isColorSupported },
         (t) => {
-          const log = messageFormat(logDescriptor, MESSAGE_KEY);
-          t.equal(log, expectedLogColored);
-          t.end();
+          const log = messageFormat(logDescriptor, MESSAGE_KEY)
+          t.equal(log, expectedLogColored)
+          t.end()
         }
-      );
+      )
 
       await t.test(
         'colors not supported in TTY',
         { skip: pretty.isColorSupported },
         (t) => {
-          const log = messageFormat(logDescriptor, MESSAGE_KEY);
-          t.equal(log, expectedLogUncolored);
-          t.end();
+          const log = messageFormat(logDescriptor, MESSAGE_KEY)
+          t.equal(log, expectedLogUncolored)
+          t.end()
         }
-      );
+      )
     }
-  );
-});
+  )
+})
 
 test('format log correctly with custom colors per level', async (t) => {
   const levels = {
     foo: 35,
-    bar: 45,
-  };
+    bar: 45
+  }
   const messageFormat = messageFormatFactory(levels, {
     35: 'bgCyanBright',
-    45: 'yellow',
-  });
+    45: 'yellow'
+  })
 
   const logCustomLevelsLogPairs = [
     [
       { time: EPOCH, level: 35, [MESSAGE_KEY]: 'basic foo log' },
       `${TIME} - \u001B[106mfoo\u001B[49m - \u001B[36mbasic foo log\u001B[39m`,
-      `${TIME} - foo - basic foo log`,
+      `${TIME} - foo - basic foo log`
     ],
     [
       {
@@ -195,13 +195,13 @@ test('format log correctly with custom colors per level', async (t) => {
         [MESSAGE_KEY]: 'basic incoming request bar log',
         req: {
           method: 'GET',
-          url: '/bar',
-        },
+          url: '/bar'
+        }
       },
       `${TIME} - \u001B[33mbar\u001B[39m - GET /bar - \u001B[36mbasic incoming request bar log\u001B[39m`,
-      `${TIME} - bar - GET /bar - basic incoming request bar log`,
-    ],
-  ];
+      `${TIME} - bar - GET /bar - basic incoming request bar log`
+    ]
+  ]
 
   await logCustomLevelsLogPairs.forEach(
     async ([logDescriptor, expectedLogColored, expectedLogUncolored]) => {
@@ -209,40 +209,40 @@ test('format log correctly with custom colors per level', async (t) => {
         'colors supported in TTY',
         { skip: !pretty.isColorSupported },
         (t) => {
-          const log = messageFormat(logDescriptor, MESSAGE_KEY);
-          t.equal(log, expectedLogColored);
-          t.end();
+          const log = messageFormat(logDescriptor, MESSAGE_KEY)
+          t.equal(log, expectedLogColored)
+          t.end()
         }
-      );
+      )
 
       await t.test(
         'colors not supported in TTY',
         { skip: pretty.isColorSupported },
         (t) => {
-          const log = messageFormat(logDescriptor, MESSAGE_KEY);
-          t.equal(log, expectedLogUncolored);
-          t.end();
+          const log = messageFormat(logDescriptor, MESSAGE_KEY)
+          t.equal(log, expectedLogUncolored)
+          t.end()
         }
-      );
+      )
     }
-  );
-});
+  )
+})
 
 test('format log correctly with custom colors per level', async (t) => {
   const levels = {
     foo: 35,
-    bar: 45,
-  };
+    bar: 45
+  }
   const messageFormat = messageFormatFactory(levels, {
     35: 'bgCyanBright',
-    45: 'yellow',
-  });
+    45: 'yellow'
+  })
 
   const logCustomLevelsLogPairs = [
     [
       { time: EPOCH, level: 35, [MESSAGE_KEY]: 'basic foo log' },
       `${TIME} - \u001B[106mfoo\u001B[49m - \u001B[36mbasic foo log\u001B[39m`,
-      `${TIME} - foo - basic foo log`,
+      `${TIME} - foo - basic foo log`
     ],
     [
       {
@@ -251,13 +251,13 @@ test('format log correctly with custom colors per level', async (t) => {
         [MESSAGE_KEY]: 'basic incoming request bar log',
         req: {
           method: 'GET',
-          url: '/bar',
-        },
+          url: '/bar'
+        }
       },
       `${TIME} - \u001B[33mbar\u001B[39m - GET /bar - \u001B[36mbasic incoming request bar log\u001B[39m`,
-      `${TIME} - bar - GET /bar - basic incoming request bar log`,
-    ],
-  ];
+      `${TIME} - bar - GET /bar - basic incoming request bar log`
+    ]
+  ]
 
   await logCustomLevelsLogPairs.forEach(
     async ([logDescriptor, expectedLogColored, expectedLogUncolored]) => {
@@ -265,21 +265,21 @@ test('format log correctly with custom colors per level', async (t) => {
         'colors supported in TTY',
         { skip: !pretty.isColorSupported },
         (t) => {
-          const log = messageFormat(logDescriptor, MESSAGE_KEY);
-          t.equal(log, expectedLogColored);
-          t.end();
+          const log = messageFormat(logDescriptor, MESSAGE_KEY)
+          t.equal(log, expectedLogColored)
+          t.end()
         }
-      );
+      )
 
       await t.test(
         'colors not supported in TTY',
         { skip: pretty.isColorSupported },
         (t) => {
-          const log = messageFormat(logDescriptor, MESSAGE_KEY);
-          t.equal(log, expectedLogUncolored);
-          t.end();
+          const log = messageFormat(logDescriptor, MESSAGE_KEY)
+          t.equal(log, expectedLogUncolored)
+          t.end()
         }
-      );
+      )
     }
-  );
-});
+  )
+})
