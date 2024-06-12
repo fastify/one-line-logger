484ae674bcecabe2cdf9c2002d5bd27a59e6fee0
Cristian Barlutiu <cristian.barlutiu@gmail.com>
2024-06-12 17:27:54 +0200

    fixed linting issues

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-'use strict';
+'use strict'
 
-const pretty = require('pino-pretty');
-const messageFormatFactory = require('./lib/messageFormatFactory');
+const pretty = require('pino-pretty')
+const messageFormatFactory = require('./lib/messageFormatFactory')
 
 const oneLineLogger = (opts = {}) => {
-  const { levels, colors, ...rest } = opts;
+  const { levels, colors, ...rest } = opts
 
   const messageFormat = messageFormatFactory(
     levels,
     colors,
     opts.colorize ?? pretty.isColorSupported
-  );
+  )
 
   return pretty({
     messageFormat,
     ignore: 'pid,hostname,time,level',
     hideObject: true,
     colorize: false,
-    ...rest,
-  });
-};
+    ...rest
+  })
+}
 
-module.exports = oneLineLogger;
-module.exports.default = oneLineLogger;
-module.exports.oneLineLogger = oneLineLogger;
+module.exports = oneLineLogger
+module.exports.default = oneLineLogger
+module.exports.oneLineLogger = oneLineLogger
 
-module.exports.messageFormatFactory = messageFormatFactory;
+module.exports.messageFormatFactory = messageFormatFactory
